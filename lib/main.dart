04f4d831db70e66ae8c47b5e6c872289b6f7a6cf
NaragudeNitin <naragudenitin@gmail.com>
2023-02-22 10:40:16 +0530

    first Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_charts/widgets/cartesianChart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Graphs',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const CartesianChartWidget(),
+    );
+  }
+}
---
+++ lib/widgets/cartesianChart.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class CartesianChartWidget extends StatefulWidget {
+  const CartesianChartWidget({super.key});
+
+  @override
+  State<CartesianChartWidget> createState() => _CartesianChartWidgetState();
+}
+
+class _CartesianChartWidgetState extends State<CartesianChartWidget> {
+  final List<SalesData> chartData = [
+    SalesData(2001, 34000, Colors.red),
+    SalesData(2002, 37000, Colors.blueAccent),
+    SalesData(2003, 3600, Colors.green),
+    SalesData(2004, 3100, Colors.orange),
+    SalesData(2005, 20000, Colors.purple )
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cartesian Charts"),
+      ),
+      body: Container(
+        height: 500,
+        margin: const EdgeInsets.all(10),
+        child: SfCartesianChart(
+          legend: Legend(isVisible: true),
+          title: ChartTitle(text: "Sales Data"),
+          series: [
+            LineSeries<SalesData, int>(
+              dashArray: <double>[10,5],
+              legendItemText: 'Sales',
+              dataSource: chartData,
+              pointColorMapper: (SalesData sales, _) => sales.color,
+              xValueMapper: (SalesData sales, _) => sales.year,
+              yValueMapper:  (SalesData sales, _) => sales.sales, 
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SalesData {
+  final int year;
+  final double sales; 
+  final Color color;
+
+  SalesData(this.year, this.sales, this.color);
+}
